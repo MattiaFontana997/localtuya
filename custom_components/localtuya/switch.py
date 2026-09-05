698ca5f21a690c0e520def73be98a0ebe719253d
Mattia Fontana <77722321+MattiaFontana997@users.noreply.github.com>
2026-09-05 22:08:01 +0200

    Use advanced mappings for switch writes

--- custom_components/localtuya/switch.py
+++ custom_components/localtuya/switch.py
@@ -4,38 +4,25 @@ import logging
 from functools import partial
 
 import voluptuous as vol
-from homeassistant.components.switch import (
-    DOMAIN,
-    SwitchDeviceClass,
-    SwitchEntity,
-)
+from homeassistant.components.switch import DOMAIN, SwitchDeviceClass, SwitchEntity
 from homeassistant.const import CONF_DEVICE_CLASS
 
 from .common import LocalTuyaEntity, async_setup_entry
 from .const import (
-    ATTR_CURRENT,
-    ATTR_CURRENT_CONSUMPTION,
-    ATTR_VOLTAGE,
-    CONF_CURRENT,
-    CONF_CURRENT_CONSUMPTION,
-    CONF_DEFAULT_VALUE,
-    CONF_PASSIVE_ENTITY,
-    CONF_RESTORE_ON_RECONNECT,
-    CONF_VOLTAGE,
+    ATTR_CURRENT, ATTR_CURRENT_CONSUMPTION, ATTR_VOLTAGE, CONF_CURRENT,
+    CONF_CURRENT_CONSUMPTION, CONF_DEFAULT_VALUE, CONF_PASSIVE_ENTITY,
+    CONF_RESTORE_ON_RECONNECT, CONF_VOLTAGE,
 )
 
 _LOGGER = logging.getLogger(__name__)
 
 
 def flow_schema(dps):
-    """Return schema used in config flow."""
     return {
         vol.Optional(CONF_CURRENT): vol.In(dps),
         vol.Optional(CONF_CURRENT_CONSUMPTION): vol.In(dps),
         vol.Optional(CONF_VOLTAGE): vol.In(dps),
-        vol.Optional(CONF_DEVICE_CLASS): vol.In(
-            [device_class.value for device_class in SwitchDeviceClass]
-        ),
+        vol.Optional(CONF_DEVICE_CLASS): vol.In([device_class.value for device_class in SwitchDeviceClass]),
         vol.Required(CONF_RESTORE_ON_RECONNECT): bool,
         vol.Required(CONF_PASSIVE_ENTITY): bool,
         vol.Optional(CONF_DEFAULT_VALUE): str,
@@ -43,92 +30,55 @@ def flow_schema(dps):
 
 
 class LocaltuyaSwitch(LocalTuyaEntity, SwitchEntity):
-    """Representation of a Tuya switch."""
-
-    def __init__(
-        self,
-        device,
-        config_entry,
-        switchid,
-        **kwargs,
-    ):
-        """Initialize the Tuya switch."""
+    def __init__(self, device, config_entry, switchid, **kwargs):
         super().__init__(device, config_entry, switchid, _LOGGER, **kwargs)
         self._state = None
-
         device_class = self._config.get(CONF_DEVICE_CLASS)
         if device_class:
             try:
                 self._attr_device_class = SwitchDeviceClass(device_class)
             except ValueError:
-                self.warning(
-                    "Ignoring unsupported switch device class %r",
-                    device_class,
-                )
+                self.warning("Ignoring unsupported switch device class %r", device_class)
 
     @property
     def is_on(self) -> bool | None:
-        """Return whether the Tuya switch is on."""
         return self._state
 
     @property
     def extra_state_attributes(self):
-        """Return legacy electrical measurements and restore state."""
         attrs = dict(super().extra_state_attributes)
-
         if self.has_config(CONF_CURRENT):
             value = self.dps(self._config[CONF_CURRENT])
             if value is not None:
                 attrs[ATTR_CURRENT] = value
-
         if self.has_config(CONF_CURRENT_CONSUMPTION):
             value = self.dps(self._config[CONF_CURRENT_CONSUMPTION])
-            if isinstance(value, (int, float)):
-                attrs[ATTR_CURRENT_CONSUMPTION] = value / 10
-            elif value is not None:
-                attrs[ATTR_CURRENT_CONSUMPTION] = value
-
+            attrs[ATTR_CURRENT_CONSUMPTION] = value / 10 if isinstance(value, (int, float)) else value
         if self.has_config(CONF_VOLTAGE):
             value = self.dps(self._config[CONF_VOLTAGE])
-            if isinstance(value, (int, float)):
-                attrs[ATTR_VOLTAGE] = value / 10
-            elif value is not None:
-                attrs[ATTR_VOLTAGE] = value
-
-        return attrs
+            attrs[ATTR_VOLTAGE] = value / 10 if isinstance(value, (int, float)) else value
+        return {key: value for key, value in attrs.items() if value is not None}
 
     def status_updated(self):
-        """Update switch state."""
         raw_state = self.dps(self._dp_id)
-
         if isinstance(raw_state, bool):
             state = raw_state
         elif raw_state in (0, 1):
             state = bool(raw_state)
         else:
             state = None
-
         self._state = state
-
         if state is not None and not self._device.is_connecting:
             self._last_state = state
 
     async def async_turn_on(self, **kwargs):
-        """Turn the Tuya switch on."""
-        await self._device.set_dp(True, self._dp_id)
+        await self.set_mapped_dp(True)
 
     async def async_turn_off(self, **kwargs):
-        """Turn the Tuya switch off."""
-        await self._device.set_dp(False, self._dp_id)
+        await self.set_mapped_dp(False)
 
     def entity_default_value(self):
-        """Return False as the default switch value."""
         return False
 
 
-async_setup_entry = partial(
-    async_setup_entry,
-    DOMAIN,
-    LocaltuyaSwitch,
-    flow_schema,
-)
+async_setup_entry = partial(async_setup_entry, DOMAIN, LocaltuyaSwitch, flow_schema)
